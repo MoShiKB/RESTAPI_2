3cb278458b3ae9d7a4867e92a77e6e735b63d551
LAPTOP-90O3I2F7\yulik <yulikeren3@gmail.com>
2026-01-25 13:56:41 +0200

    added create post option and test

--- controller/posts.controller.ts
+++ controller/posts.controller.ts
@@ -2,3 +2,18 @@ import { Request, Response } from "express";
 import postModel from "../model/posts.model";
 
 
+const createPost = async (req: Request, res: Response): Promise<void> => {
+    const postData = req.body;
+    console.log(postData);
+    try {
+        const newPost = await postModel.create(postData);
+        res.status(201).json(newPost);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Failed to create post" });
+    }
+};
+
+export default {
+    createPost,
+};
--- routes/posts.routes.ts
+++ routes/posts.routes.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import postsController from "../controller/posts.controller";
+
 const router = express.Router();
+
+router.post("/", postsController.createPost);
 
 export default router;
